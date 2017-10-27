7f217ef449f9f788e21fdb41588e835f9d043dd6
Chris Schlereth <contact@chris-schlereth.com>
2017-10-27 16:07:20 -0600

    Added camera controller class and manager

---
+++ Assets/GameLogic/Cameras/CameraControllerManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraControllerManager : MonoBehaviour
+{
+    //==============================================================================
+    //
+    //                                    PROPERTIES
+    //
+    //==============================================================================
+
+
+
+    protected _BaseCameraController _CurrentCameraController = null;
+    
+    /// <summary>
+    /// The active camera controller assigned to this manager
+    /// </summary>
+    public _BaseCameraController CurrentCameraController
+    {
+        get { return this._CurrentCameraController; }
+        set { this._CurrentCameraController = value; }
+    }
+
+
+
+
+
+    //==============================================================================
+    //
+    //                                UNITY LIFECYCLE
+    //
+    //==============================================================================
+
+
+
+    // Use this for initialization
+    void Start () {
+        _CurrentCameraController = this.GetComponent<DefaultCameraController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/GameLogic/Cameras/_BaseCameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class _BaseCameraController : MonoBehaviour
+{
+    //==============================================================================
+    //
+    //                                    PROPERTIES
+    //
+    //==============================================================================
+
+
+    /// <summary>
+    /// Reference to the camera that this controller is managing
+    /// </summary>
+    public Camera Camera {
+        get { return this._Camera; }
+    }
+
+    protected Camera _Camera = null;
+
+
+
+    protected Transform _XForm_CameraRoot = null;
+
+    public Transform XForm_CameraRoot
+    {
+        get { return this._XForm_CameraRoot; }
+    }
+
+
+
+
+    //==============================================================================
+    //
+    //                                UNITY LIFECYCLE
+    //
+    //==============================================================================
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
