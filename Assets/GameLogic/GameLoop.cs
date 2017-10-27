c3dd72709a1f20111faa6572b215d4008f4b9a58
Chris Schlereth <contact@chris-schlereth.com>
2017-10-27 16:09:25 -0600

    Added default camera controller, a red lens flare, and established a bounding box for the scale of the scene.

--- Assets/GameLogic/GameLoop.cs
+++ Assets/GameLogic/GameLoop.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameLoop : MonoBehaviour {
+public class GameLoop : MonoBehaviour
+{
+    //==============================================================================
+    //
+    //                                    PROPERTIES
+    //
+    //==============================================================================
 
-	// Use this for initialization
-	void Start () {
+
+
+    public static GameLoop MAIN
+    {
+        get { return GameLoop._MAIN; }
+    }
+
+    private static GameLoop _MAIN = null;
+
+
+
+    public Assets.GameLogic.Input.InputManager InputManager = null;
+
+
+
+
+
+
+    //==============================================================================
+    //
+    //                                UNITY LIFECYCLE
+    //
+    //==============================================================================
+
+
+
+    // Use this for initialization
+    void Start () {
         Debug.Log("Hello, Space Universe!");
+
+        _MAIN = this;
+
+        if (InputManager == null)
+        {
+            this.GetComponent<Assets.GameLogic.Input.InputManager>();
+
+            if (InputManager == null)
+                Debug.LogError("Could not load an Input Manager for the project.");
+        }
 	}
 	
+
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        //Update the state of all input keys the game is tracking.
+        InputManager?.UpdateInputKeys();
 	}
 }
