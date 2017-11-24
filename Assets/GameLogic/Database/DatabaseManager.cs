3832838cc86f87e6d791c93bd320fa3fdb302564
Chris Schlereth <contact@chris-schlereth.com>
2017-11-24 15:18:58 -0700

    Added SQLite Database for loading in persistant pilot data (more things coming soon)

---
+++ Assets/GameLogic/Database/DatabaseManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+namespace Assets.GameLogic.Database
+{
+    public static class DatabaseManager
+    {
+        //==============================================================================
+        //
+        //                                    CONSTANTS
+        //
+        //==============================================================================
+
+
+
+        /// <summary>
+        /// The path to the database
+        /// </summary>
+        private static readonly string DATABASE_PATH = $"URI=file:{Application.dataPath}/StreamingAssets/Database.db";
+
+
+
+
+
+        //==============================================================================
+        //
+        //                                    PROPERTIES
+        //
+        //==============================================================================
+
+
+
+        public static IDbConnection Conn = null;
+
+
+
+
+
+
+
+
+        //==============================================================================
+        //
+        //                                    METHODS
+        //
+        //==============================================================================
+
+
+
+        public static bool IsConnected
+        {
+            get { return Connect(); }
+        }
+
+
+
+        public static bool Connect()
+        {
+            if (Conn != null)
+            {
+                if(Conn.State == ConnectionState.Open)
+                    return true;
+                else if(Conn.State == ConnectionState.Closed)
+                {
+                    return _TryOpenDB();
+                }
+            }
+            else
+            {
+                Conn = new SqliteConnection(DATABASE_PATH);
+                return _TryOpenDB();
+            }
+
+            return false;
+        }
+
+
+
+        private static bool _TryOpenDB()
+        {
+            try
+            {
+                if (Conn != null)
+                    Conn.Open();
+
+                return Conn.State == ConnectionState.Open;
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"Could not open SQLite database at location:'{DATABASE_PATH}'!");
+                return false;
+            }
+        }
+
+
+
+        public static void Close()
+        {
+            if (Conn != null)
+            {
+                Conn.Close();
+                Conn = null;
+            }
+        }
+    }
+}
---
+++ Assets/GameLogic/Database/Tables/Pilots.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+namespace Assets.GameLogic.Database.Tables
+{
+    public static class PilotTable
+    {
+        //==============================================================================
+        //
+        //                                    METHODS
+        //
+        //==============================================================================
+
+
+
+        public static List<Data.Pilots._Pilot_Serialized> ReadPlayerPilots(int inMaxPilots = 100000)
+        {
+            List<Data.Pilots._Pilot_Serialized> OutPilots = new List<Data.Pilots._Pilot_Serialized>();
+
+            if (DatabaseManager.IsConnected)
+            {
+                using (IDbCommand Cmd = DatabaseManager.Conn.CreateCommand())
+                {
+                    if(inMaxPilots < 1)
+                        inMaxPilots = Math.Abs(inMaxPilots);
+
+                    Cmd.CommandText = $"SELECT Name_First, Name_Last, Callsign, Level, Level_XPThisLevel, Gender, ShipIDPreferences FROM Pilot WHERE 1 ORDER BY Level DESC, Level_XPThisLevel DESC LIMIT {inMaxPilots};";
+
+                    using (IDataReader Rdr = Cmd.ExecuteReader())
+                    {
+                        while(Rdr.Read())
+                        {
+                            Data.Pilots._Pilot_Serialized iPilot = new Data.Pilots._Pilot_Serialized();
+
+                            iPilot.Name_First = Rdr.GetString(Rdr.GetOrdinal("Name_First"));
+                            iPilot.Name_Last = Rdr.GetString(Rdr.GetOrdinal("Name_Last"));
+
+                            if (!Rdr.IsDBNull(Rdr.GetOrdinal("Callsign")))
+                                iPilot.CallSign = Rdr.GetString(Rdr.GetOrdinal("Callsign"));
+
+                            iPilot.Level = Rdr.GetInt32(Rdr.GetOrdinal("Level"));
+                            iPilot.Level_XPThisLevel = Rdr.GetInt32(Rdr.GetOrdinal("Level_XPThisLevel"));
+                            iPilot.Gender = Rdr.GetByte(Rdr.GetOrdinal("Gender"));
+
+                            if (!Rdr.IsDBNull(Rdr.GetOrdinal("ShipIDPreferences")))
+                                iPilot.CallSign = Rdr.GetString(Rdr.GetOrdinal("ShipIDPreferences"));
+
+                            OutPilots.Add(iPilot);
+                        }
+                    }
+                }
+            }
+
+            return OutPilots;
+        }
+    }
+}
